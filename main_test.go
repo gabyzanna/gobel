aaba94cb32ed49edc151ebf219c6530a69b9ddef
Gabriella Youzanna Rorong <gabriellayouzanna@gmail.com>
2023-11-14 18:25:09 +0700

    tes

---
+++ main_test.go
@@ -0,0 +1,15 @@
+package gobel
+
+import (
+	"fmt"
+	"testing"
+	"github.com/whatsauth/watoken"
+)
+ 
+func TestGeneratePrivateKeyPaseto(t *testing.T) {
+	privateKey, publicKey := watoken.GenerateKey()
+	fmt.Println(privateKey)
+	fmt.Println(publicKey)
+	hasil, err := watoken.Encode("belcoba", privateKey)
+	fmt.Println(hasil, err)
+}
